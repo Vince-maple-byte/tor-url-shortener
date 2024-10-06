063b5556ae287d9f0c8e97f6cf4c07d09731fc50
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2024-10-06 01:54:21 -0400

    Changed a test so that it can run with a redis mock

--- tor-url-shortener/src/test/java/com/vince/tor_url_shortener/service/UrlEncoderImplTest.java
+++ tor-url-shortener/src/test/java/com/vince/tor_url_shortener/service/UrlEncoderImplTest.java
@@ -1,23 +1,37 @@
 package com.vince.tor_url_shortener.service;
 
 import com.vince.tor_url_shortener.repository.UrlRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class UrlEncoderImplTest {
 
-    private final UrlRepository urlRepository;
-    private final RedisCounter redisCounter;
+    @Mock
+
+    private UrlRepository urlRepository;
+
+    @Mock
+    private RedisCounter redisCounter;
 
 
-    @Autowired
-    public UrlEncoderImplTest(UrlRepository urlRepository, RedisCounter redisCounter) {
-        this.urlRepository = urlRepository;
-        this.redisCounter = redisCounter;
+    @InjectMocks
+    private UrlEncoderImpl urlEncoder;
+
+    @BeforeEach
+    void setUp() {
+        // Mock the RedisCounter behavior (e.g., mock getCounter to return 100)
+        Mockito.when(redisCounter.getCounterAndIncrement()).thenReturn(1L);
     }
 
     public
@@ -29,7 +43,7 @@ class UrlEncoderImplTest {
         UrlEncoderImpl urlEncoder = new UrlEncoderImpl(base62, urlRepository, redisCounter);
         String result = urlEncoder.encode("JJJ");
         //Then
-        String expected = "100";
+        String expected = "1";
         assertEquals(expected, result);
     }
 
