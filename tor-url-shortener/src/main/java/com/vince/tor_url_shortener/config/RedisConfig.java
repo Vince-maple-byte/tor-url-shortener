3340b3c9df32d02587068f0d3c48e611d0f6308c
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2025-08-18 02:34:20 -0400

    Finished with most of the dockerization of the project just need to figure out how to fix the redis connection of the project and thymeleaf

--- tor-url-shortener/src/main/java/com/vince/tor_url_shortener/config/RedisConfig.java
+++ tor-url-shortener/src/main/java/com/vince/tor_url_shortener/config/RedisConfig.java
@@ -1,20 +1,34 @@
 package com.vince.tor_url_shortener.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    //TODO: FIX THE REDIS CONNECTION TO WORK FOR DOCKER
+//    @Value("${spring.data.redis.host}")
+//    private String hostName;
+//
+//    @Value("${spring.data.redis.port}")
+//    private int port;
+//
+//
+//    @Bean
+//    public LettuceConnectionFactory lettuceConnectionFactory() {
+//        // ! You have to provide the redisStandaloneConfiguration or else the app wont
+//        // ! work with docker
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+//        redisStandaloneConfiguration.setHostName(hostName);
+//        redisStandaloneConfiguration.setPort(port);
+//        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+//    }
 
-
-    @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
-    }
     @Bean
     public RedisTemplate<String, Long> redisTemplateCounter(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Long> template = new RedisTemplate<>();
